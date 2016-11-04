ad9c96659a545219ac4833e059c9a1feeb65503b
ciortanmadalina <ciortanmadalina@gmail.com>
2016-11-04 20:09:02 +0100

    filter duplicates

--- a.r
+++ a.r
@@ -10,18 +10,31 @@ average <- function(v){
 
 averageApproach1 <- average(frequency)
 averageApproach2 <- mean(frequency)
-
 all (averageApproach1 == averageApproach2)
 
+#But if we consider that input data gives us duplicated values for the pair geneId - transcriptId 
+#due to other factors, we only want to keep the unique combinations
+#For instance:
+#ENSG00000100023,ENST00000335025,22,21652270,21700015,1,54.20,NM_148176,PPIL2
+#ENSG00000100023,ENST00000335025,22,21652270,21700015,1,54.20,NM_148175,PPIL2
+#ENSG00000100023,ENST00000398831,22,21652270,21700015,1,54.20,,PPIL2
+#should only count ENST00000335025 and ENST00000398831 for ENSG00000100023
+
+noDuplicatesData <-unique(ensembl[c("Ensembl.Gene.ID", "Ensembl.Transcript.ID")])
+frequencyNoDuplicates<-table(as.vector(noDuplicatesData$Ensembl.Gene.ID))
+averageNoDuplicates <- mean(frequencyNoDuplicates)
 #What is the maximum, minimum and average number of transcripts per gene?
 
 min(frequency)
 max(frequency)
 mean(frequency)
 
+min(frequencyNoDuplicates)
+max(frequencyNoDuplicates)
+mean(frequencyNoDuplicates)
 
 #Plot the number of transcript per gene distribution (histogram)
-frequencyHistogram = as.vector(frequency)
+frequencyHistogram = as.vector(frequencyNoDuplicates)
 
 pdf("a.pdf")
 
